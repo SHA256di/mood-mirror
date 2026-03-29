8d981603a3d89a766302dee20bec81c77d3bd0ad
SHA256wdi <shawgaze@gmail.com>
2026-03-28 22:28:53 -0700

    chore: update CORS allow origins

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://querate-ai.vercel.app", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
